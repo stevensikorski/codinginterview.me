cbc384d724eff09b70cad0df7f7839bde73d26fd
Asafe M Brandao <asafemartins1919@gmail.com>
2025-04-07 00:07:45 -0400

    landing page adjustments

--- src/components/Home/HowItWorks.jsx
+++ src/components/Home/HowItWorks.jsx
@@ -1,25 +1,25 @@
 export default function HowItWorks() {
   return (
-    <section className="px-4 py-14 bg-dark">
-      <div className="container mx-auto">
-        <div className="max-w-[1145px] mx-auto space-y-4">
-          <h2 className="text-center text-[25px] text-light font-bold">
-            How it works
-          </h2>
-          <ul className="grid gap-0.5 list-decimal">
-            <li className="text-light text-2xl font-bold ">
-               Find a Partner – <span className="font-normal"> Connect with a friend or
-              another student looking to practice coding interviews.</span>
-            </li>
-            <li className="text-light text-2xl font-bold">
-              Start a Live Interview - <span className="font-normal">  One person acts as the interviewer while the other takes on the role of the interviewee.</span>
-            </li>
-            <li className="text-light text-2xl font-bold">
-            Solve Real Problems – <span className="font-normal">  The interviewer selects a coding problem from a curated pool, and the interviewee solves it in a cloud-based IDE while communicating over video.</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
+   <section className="px-4 py-14 bg-dark">
+     <div className="container mx-auto">
+       <div className="max-w-[1145px] mx-auto space-y-4">
+         <h2 className="text-center text-[25px] text-light font-bold">
+           How it works
+         </h2>
+         <ul className="grid gap-0.5 list-decimal">
+           <li className="text-light text-2xl font-bold ">
+             Find a Partner – <span className="font-normal"> Connect with a friend or
+             another student looking to practice coding interviews.</span>
+           </li>
+           <li className="text-light text-2xl font-bold">
+             Start a Live Interview - <span className="font-normal"> One person acts as the interviewer while the other takes on the role of the interviewee.</span>
+           </li>
+           <li className="text-light text-2xl font-bold">
+             Solve Real Problems – <span className="font-normal"> The interviewer selects a coding problem from a curated pool, and the interviewee solves it in a cloud-based IDE while communicating over video.</span>
+           </li>
+         </ul>
+       </div>
+     </div>
+   </section>
   );
-}
+ }
