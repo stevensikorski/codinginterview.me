85466df79bea5acb0707aa72ca558fe98d3cc6f5
stevensikorski <ssikorski03@gmail.com>
2025-04-07 00:05:13 -0400

    revert

--- src/backend/config_files/firebase-admin-config.js
+++ src/backend/config_files/firebase-admin-config.js
@@ -1,28 +1,15 @@
 import admin from "firebase-admin";
 import { getAuth } from "firebase-admin/auth";
-import fs from "fs";
 
-const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+// Get service account
+const service_account = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-if (!keyPath) {
-  console.error("GOOGLE_APPLICATION_CREDENTIALS is not defined");
-  process.exit(1);
-}
-
-let serviceAccount;
-try {
-  serviceAccount = JSON.parse(fs.readFileSync(keyPath, "utf8"));
-} catch (error) {
-  console.error("Error reading or parsing service account file:", error);
-  process.exit(1);
-}
-
-const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+// Initialize Firebase
+const firebase_admin = admin.initializeApp({
+  credential: admin.credential.cert(service_account),
   databaseURL: "https://codinginterview-me-default-rtdb.firebaseio.com",
 });
 
-const auth = getAuth(firebaseAdmin);
-const rtdb = firebaseAdmin.database();
-
+const auth = getAuth(firebase_admin);
+const rtdb = firebase_admin.database();
 export { auth, rtdb };
